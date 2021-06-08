973b58cd22e4a189686dfa5c6dc2ca8b2fa63145
Karkuvelraja Thangamariappan (Raja) <85283603+tkarkuvelraja@users.noreply.github.com>
2021-06-09 01:27:58 +0400

    isNumberKey.js

---
+++ isNumberKey.js
@@ -0,0 +1,34 @@
+/*
+ * Number Field.
+ * Plug-in Type: Itenm
+ * Summary: Plugin In to allow only Numeric Values and allow Only one DOT in a Number Field.
+ *
+ * ----- Contact information ----
+ *
+ * Developed by * Evaluate Consulting Services, Bangalore
+ * Contact: info@evaluatecs.com
+ * Website: https://evaluatecs.com
+ *
+ * @author Karkuvelraja Thangamariappan - tkarkuvelraja@gmail.com
+ * Profile: https://www.linkedin.com/in/karkuvelrajathangamariappan
+ * Twitter: https://twitter.com/tkarkuvelraja
+ * Blog: http://orclking.blogspot.com/
+ */
+ 
+function isNumberKey(element) {
+    var keyPressed = (event.which) ? event.which : (window.event.keyCode) ? window.event.keyCode : -1;
+	console.log (keyPressed); 
+    if ((keyPressed == 45) && (keyPressed >= 48) && (keyPressed <= 57)) {
+        return true;
+    }
+
+    // '.' checking decimal point...
+    else if (keyPressed == 46) {
+        // Allow only 1 decimal point ('.')...  
+        if ((element.value) && (element.value.indexOf('.') >= 0))
+            return false;
+        else
+            return true;
+    }
+    return false;
+}
